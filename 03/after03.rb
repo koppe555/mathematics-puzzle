04b6ac5724fe5a68d4c8f9937068f4d1e465d700
koppe555 <kohei.ishibashi55@gmail.com>
2020-08-04 12:19:36 +0900

    2020/08/04

---
+++ 03/after03.rb
@@ -0,0 +1,21 @@
+# カードの初期化
+N = 100
+cards = Array.new(N, false)
+
+# 2〜Nまで裏返す
+(2..N).each{|i|
+  j = i - 1
+  p "これはj"
+  puts j
+  while (j < cards.size) do
+    p "これはこれはj"
+    puts j
+    cards[j] = !cards[j]
+    j += i
+  end
+}
+
+# 裏向きのカードを出力
+N.times{|i|
+  puts i + 1 if !cards[i]
+}
